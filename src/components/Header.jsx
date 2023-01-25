e142774c75841b1185499c5de9b341d8a3f2ac9e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-25 16:16:55 +0800

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/images/logo.svg";
 
 const Header = () => {
-  return (
-    <nav>
-      <ul>
-        <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/characters"}>Characters</Link></li>
-        <li><Link to={"/episodes"}>Epsodes</Link></li>
-        <li><Link to={"/locations"}>Locations</Link></li>
-      </ul>
-    </nav>
-  )
-}
+  const rootElementClassList = document.documentElement.classList;
+  const isDark = rootElementClassList.contains("dark") ? true : false;
+  const [dark, setDark] = useState(isDark);
+  const [open, setOpen] = useState(false);
+  const iconClass = `material-icons-outlined ${
+    dark ? "text-gray-50" : "text-gray-900"
+  }`;
 
-export default Header
+  function toggleColorTheme() {
+    setDark(!dark);
+    rootElementClassList.toggle("dark");
+  }
+
+  return (
+    <header className="p-6 flex justify-between items-center gap-2">
+      <div className="flex justify-between w-full items-center">
+        <Link to={"/"}>
+          <p className="font-schwifty text-lg font-bold">
+            R<span className="text-xs">ick</span>{" "}
+            <span className="text-[8px]">and</span> M
+            <span className="text-xs">orty</span>
+          </p>
+        </Link>
+        <button onClick={toggleColorTheme} className="z-0 flex">
+          <span className={iconClass}>{dark ? `light_mode` : `dark_mode`}</span>
+        </button>
+      </div>
+
+      <button onClick={() => setOpen(!open)} className="z-20 flex">
+        <span className={iconClass}>{open ? `close` : `menu`}</span>
+      </button>
+
+      <nav
+        className={`z-10 bg-slate-100 dark:bg-gray-800 fixed inset-y-0 right-0 w-2/3 transition-all ${
+          open ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/characters"}>Characters</Link>
+          </li>
+          <li>
+            <Link to={"/episodes"}>Epsodes</Link>
+          </li>
+          <li>
+            <Link to={"/locations"}>Locations</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <>
     <Header />
-    <div>Home</div>
+    <Hero />
+    <div className="font-schwifty">Home</div>
     </>
   )
 }
