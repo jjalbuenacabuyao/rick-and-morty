3dffa850d44f600390f41fc277f6a6a85da28f38
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-25 17:33:16 +0800

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import heroImg from "../assets/images/hero.png";
 
 const Hero = () => {
   return (
-    <div></div>
-  )
-}
+    <section className="pt-8 flex flex-col gap-10">
+      <article className="px-6 text-center flex flex-col gap-5 items-center">
+        <h1 className="text-2xl font-bold">
+          Welcome to the website of <br />{" "}
+          <span className="font-schwifty text-4xl leading-[2em]">
+            Rick and Morty
+          </span>
+        </h1>
+        <p>
+          The hit Adult Swim show that follows the adventures of mad scientist
+          Rick Sanchez and his good-hearted but easily influenced grandson Morty
+          Smith.
+        </p>
+        <a
+          href="https://www.adultswim.com/videos/rick-and-morty"
+          className="font-bold py-2 px-6 rounded-lg w-max border-2 border-custom-black dark:border-gray-200"
+        >
+          Learn More
+        </a>
+      </article>
 
-export default Hero
+      <picture>
+        <img src={heroImg} alt="Rick and Morty coming out from a portal" />
+      </picture>
+    </section>
+  );
+};
+
+export default Hero;
