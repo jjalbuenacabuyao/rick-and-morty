60190286b92b4d25789a90436de210cef4cf8638
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-30 21:51:48 +0800

    imported and exported Showcase and Filter

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Hero from "./Hero";
 import Header from "./Header";
+import Filter from "./Filter";
+import Showcase from "./Showcase";
 
 export {
   Header,
-  Hero
+  Hero,
+  Filter,
+  Showcase
 }
