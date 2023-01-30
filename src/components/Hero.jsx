4716f43a47bd32490864a88fffcffb50b12a2618
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-30 13:33:43 +0800

    created hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,55 @@
 import React from "react";
-import heroImg from "../assets/images/hero.png";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return (
-    <section className="flex flex-col gap-10 pt-8">
-      <article className="flex flex-col items-center gap-5 px-6 text-center">
-        <h1 className="text-2xl font-bold">
-          Welcome to the website of <br />{" "}
-          <span className="font-schwifty text-4xl leading-[2em]">
-            Rick and Morty
-          </span>
-        </h1>
-        <p>
-          The hit Adult Swim show that follows the adventures of mad scientist
-          Rick Sanchez and his good-hearted but easily influenced grandson Morty
-          Smith.
-        </p>
-        <a
-          href="https://www.adultswim.com/videos/rick-and-morty"
-          className="w-max rounded-lg border-2 border-custom-black py-2 px-6 font-bold dark:border-gray-200"
-        >
-          Learn More
-        </a>
-      </article>
+  const links = [
+    {
+      id: "Characters",
+      link: "/characters",
+    },
+    {
+      id: "Episodes",
+      link: "/episodes",
+    },
+    {
+      id: "Locations",
+      link: "/locations",
+    },
+  ];
 
-      <picture>
-        <img src={heroImg} alt="Rick and Morty coming out from a portal" />
-      </picture>
-    </section>
+  const listItems = links.map((link) => (
+    <li
+      key={link.id}
+      className="justify-self-center rounded-md bg-gray-900 py-2 px-4 text-[10px] uppercase leading-none text-gray-50 dark:bg-gray-800"
+    >
+      <Link to={link.link}>{link.id}</Link>
+    </li>
+  ));
+
+  return (
+    <article className="flex h-screen flex-col items-center justify-center gap-4 px-6">
+      <h1 className="px-2 text-center text-4xl font-bold leading-[1.2em]">
+        Rick and Morty Wiki
+      </h1>
+      <p>
+        Built using the{" "}
+        <a
+          href="https://rickandmortyapi.com/"
+          target="_blank"
+          className="font-bold text-sky-500 underline decoration-sky-800 decoration-wavy decoration-1 underline-offset-4 dark:text-sky-400 dark:decoration-sky-300"
+        >
+          Rick and Morty API
+        </a>
+        .
+      </p>
+      <ul className="flex justify-center gap-2">{listItems}</ul>
+      <a
+        href="https://github.com/jjalbuenacabuyao/rick-and-morty"
+        className="flex items-center gap-4 rounded-md bg-gray-900 py-2 px-4 text-[10px] uppercase leading-none text-gray-50 dark:bg-gray-800"
+      >
+        <span>Source Code</span>
+      </a>
+    </article>
   );
 };
 
