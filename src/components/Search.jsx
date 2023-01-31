543d808883d5c73cb97aa6179ca0bbb5873d7781
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-31 13:53:10 +0800

    changed the border color and text color in dark mode

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -10,13 +10,13 @@ const Search = ({ setFilters }) => {
           name="search"
           id="search"
           placeholder="Search"
-          className="relative w-full rounded-full border-2 border-gray-500 py-2 px-4 text-sm focus:border-gray-900 focus:outline-none"
+          className="relative w-full rounded-full border-2 bg-transparent border-gray-500 dark:border-sky-400 py-2 px-4 text-sm focus:border-gray-900 focus:outline-none"
           autoComplete="off"
           onChange={(e) => {
             setFilters({ ...filters, name: e.target.value });
           }}
         />
-        <span className="material-icons absolute right-4 top-0 bottom-0 my-auto aspect-square w-6 leading-none text-gray-600">
+        <span className="material-icons absolute right-4 top-0 bottom-0 my-auto aspect-square w-6 leading-none text-gray-600 dark:text-gray-200">
           search
         </span>
       </label>
