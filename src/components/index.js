d432f118da5ca093fb6e8b490a4c6acac948d152
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-31 11:57:52 +0800

    exported Box

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Filter from "./Filter";
 import Showcase from "./Showcase";
 import Search from "./Search";
+import Box from "./Box";
 
 export {
   Header,
   Hero,
   Filter,
   Showcase,
-  Search
+  Search,
+  Box
 }
