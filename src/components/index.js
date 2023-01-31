cd7bae666d3402c445fe87a5420ca8aa1ec5f64b
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-31 11:57:11 +0800

    imported and exported Searc

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Hero from "./Hero";
 import Header from "./Header";
 import Filter from "./Filter";
 import Showcase from "./Showcase";
+import Search from "./Search";
 
 export {
   Header,
   Hero,
   Filter,
-  Showcase
+  Showcase,
+  Search
 }
