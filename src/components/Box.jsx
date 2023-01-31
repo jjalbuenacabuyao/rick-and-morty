3b16643b939039db5acefefa6fc9a95446b48637
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-31 13:49:09 +0800

    initial commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Filter, Search, Showcase, Header} from "../components"
+
+const Box = ({title, setPage, apiData, setFilters}) => {
+  return (
+    <>
+      <Header />
+      <div className="px-6 pt-28 flex flex-col gap-10">
+        <h1 className="text-center text-2xl font-bold text-sky-900 dark:text-sky-400">{title}</h1>
+        <Search setFilters={setFilters} />
+
+        <div>
+          <Filter setFilters={setFilters} />
+          <Showcase apiData={apiData} />
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+export default Box
