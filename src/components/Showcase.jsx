944e4f3d99780e5e8c1e550a07c09b502ca3784e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-01-31 21:39:32 +0800

    initial test

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const Showcase = () => {
-  return (
-    <div>Showcase</div>
-  )
-}
+const Showcase = ({ apiData }) => {
+  let characterCard = apiData.map((character) => (
+    <div key={character.id} className="border-2 border-gray-700 rounded-lg">
+      <div>
+        <img src={character.image} alt="" className="rounded-lg"/>
+        <p>{character.status}</p>
+      </div>
+      
+      <p>{character.name}</p>
+    </div>
+  ));
+  return <div className="grid grid-cols-2 gap-5">{characterCard}</div>;
+};
 
-export default Showcase
+export default Showcase;
