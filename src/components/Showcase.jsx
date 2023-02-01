f8488be3186450cd6ad3fdc360eabcf2003a7724
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-01 10:44:46 +0800

    collect necessary character info

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
 const Showcase = ({ apiData }) => {
-  let characterCard = apiData.map((character) => (
-    <div key={character.id} className="border-2 border-gray-700 rounded-lg">
-      <div>
-        <img src={character.image} alt="" className="rounded-lg"/>
-        <p>{character.status}</p>
-      </div>
-      
-      <p>{character.name}</p>
+  return apiData === undefined ? (
+    <span className="text-center block">Character doesn't exist</span>
+  ) : (
+    <div className="grid grid-cols-2 gap-5">
+      {
+        (apiData.map((character) => (
+          <div
+            key={character.id}
+            className="rounded-lg border-2 border-gray-700"
+          >
+            <div>
+              <img src={character.image} alt="" className="rounded-lg" />
+              <p>{character.status}</p>
+            </div>
+
+            <p>{character.name}</p>
+            <p>{character.gender}</p>
+            <p>{character.location.name}</p>
+          </div>
+        )))
+      }
     </div>
-  ));
-  return <div className="grid grid-cols-2 gap-5">{characterCard}</div>;
+  );
 };
 
 export default Showcase;
