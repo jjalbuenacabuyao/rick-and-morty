825a6231b7830645a25428b9c3884a801bd4168f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-02 16:15:15 +0800

    styled the buttons and the fieldsets

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 const Filter = ({ filters, setFilters }) => {
   const [collapse, setCollapse] = useState(false);
-  const [statusSelected, setStatusSelected] = useState("");
-  const [genderSelected, setGenderSelected] = useState("");
-  const [speciesSelected, setSpeciesSelected] = useState("");
+  const [statusSelected, setStatusSelected] = useState("unset");
+  const [genderSelected, setGenderSelected] = useState("unset");
+  const [speciesSelected, setSpeciesSelected] = useState("unset");
   const filterTags = ["status", "gender", "species"];
   const choices = {
     status: ["alive", "dead", "unknown", "unset"],
@@ -27,15 +27,15 @@ const Filter = ({ filters, setFilters }) => {
 
   const fieldsets = filterTags.map((filter) => (
     // Map each items in the array and creates fieldset. Inside the fieldset is a paragraph containing and the generated labels from choices object.
-    <fieldset key={filter}>
-      <p>{filter}</p>
+    <fieldset key={filter} className="flex gap-3 flex-wrap capitalize items-center">
+      <p className="text-lg font-bold leading-none">{filter}:</p>
       {filter === "status"
         ? choices.status.map((status) => (
             <label
               key={status}
               className={`${
-                statusSelected === status ? "border-[1px] border-gray-900" : ""
-              }`}
+                statusSelected === status ? "text-gray-50 bg-gray-800 rounded-full text-sm" : ""
+              } px-2 py-1`}
             >
               <span>{status}</span>
               <input
@@ -57,8 +57,8 @@ const Filter = ({ filters, setFilters }) => {
             <label
               key={gender}
               className={`${
-                genderSelected === gender ? "border-[1px] border-gray-900" : ""
-              }`}
+                genderSelected === gender ? "text-gray-50 bg-gray-800 rounded-full text-sm" : ""
+              } px-2 py-1`}
             >
               <span>{gender}</span>
               <input
@@ -79,8 +79,8 @@ const Filter = ({ filters, setFilters }) => {
             <label
               key={species}
               className={`${
-                speciesSelected === species ? "border-[1px] border-gray-900" : ""
-              }`}
+                speciesSelected === species ? "text-gray-50 bg-gray-800 rounded-full text-sm" : ""
+              } px-2 py-1`}
             >
               <span>{species}</span>
               <input
@@ -100,15 +100,13 @@ const Filter = ({ filters, setFilters }) => {
     </fieldset>
   ));
 
-  console.log(filters);
-
   return (
-    <div>
+    <div className="relative">
       {/* Toggle Button */}
       <button
         aria-expanded={collapse}
         onClick={() => setCollapse(!collapse)}
-        className="flex items-center justify-between gap-2 rounded-md border-[1px] border-gray-900 px-3 py-1"
+      className={`flex items-center justify-between gap-2 rounded-md  border-gray-900 px-3 py-1 ${collapse ? "border-2" : "border-[1px]"}`}
       >
         <span className="text-sm leading-none">Filters</span>
         <span className="material-icons-outlined">
@@ -117,7 +115,7 @@ const Filter = ({ filters, setFilters }) => {
       </button>
 
       {/* Filters */}
-      <form className={`overflow-hidden ${collapse ? "h-full opacity-animation" : "h-0"}`}>{fieldsets}</form>
+      <form className={`overflow-hidden ${collapse ? "h-auto opacity-animation rounded-lg p-2 border-gray-400 border-[1px]" : "h-0"} bg-gray-100 flex flex-col gap-5 absolute top-10 right-0 left-0 z-10`}>{fieldsets}</form>
     </div>
   );
 };
