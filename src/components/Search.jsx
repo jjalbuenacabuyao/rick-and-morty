3ef9b0abe57199c05ff813b72437d3641f55cdf0
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-02 09:40:57 +0800

    fix the value and props

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Search = ({ filters, setFilters }) => {
+const Search = ({setSearch }) => {
   return (
     <form>
       <label htmlFor="search" className="relative block">
@@ -13,7 +13,7 @@ const Search = ({ filters, setFilters }) => {
           className="relative w-full rounded-full border-2 bg-transparent border-gray-500 dark:border-sky-400 py-2 px-4 text-sm focus:border-gray-900 focus:outline-none"
           autoComplete="off"
           onChange={(e) => {
-            setFilters({ ...filters, name: e.target.value });
+            setSearch(e.target.value);
           }}
         />
         <span className="material-icons absolute right-4 top-0 bottom-0 my-auto aspect-square w-6 leading-none text-gray-600 dark:text-gray-200">
