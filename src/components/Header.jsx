75e015987f27ccb8221d0d79a3ad561989457bc9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-04 17:08:55 +0800

    styled the nav and change the appearance in medium screen devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,7 @@ const Header = () => {
   }`;
 
   function toggleColorTheme() {
+    theme === "dark" ? window.localStorage.setItem("darkmode", "disabled") : window.localStorage.setItem("darkmode", "enabled")
     setTheme(theme === "dark" ? "light" : "dark");
   }
 
@@ -47,13 +48,16 @@ const Header = () => {
 
   const listItems = links.map((link) => (
     <li key={link.id}>
-      <Link to={link.link}>{link.id}</Link>
+      <Link 
+        to={link.link}
+        className={`${window.location.pathname === link.link ? "text-sky-400 font-bold " : ""} transition-all`}
+      >{link.id}</Link>
     </li>
   ));
 
   return (
     <header className="absolute right-0 left-0 top-0 flex max-w-screen-2xl items-center justify-between gap-2 px-36-140 pt-6 2xl:m-auto">
-      <div className="flex w-full items-center justify-between">
+      <div className="flex w-full items-center justify-between md:pr-3 md:border-r-2 md:border-r-gray-400 md:dark:border-r-gray-600">
         <Link to={"/"}>
           <p className="font-schwifty text-lg">
             R<span className="text-xs">ick</span>{" "}
@@ -68,16 +72,16 @@ const Header = () => {
         </button>
       </div>
 
-      <button onClick={() => setOpen(!open)} className="z-20 flex">
+      <button onClick={() => setOpen(!open)} className="z-20 flex md:hidden">
         <span className={iconClass}>{open ? `close` : `menu`}</span>
       </button>
 
       <nav
-        className={`fixed inset-y-0 right-0 z-10 w-2/3 bg-slate-100 transition-all dark:bg-gray-800 ${
+        className={`fixed inset-y-0 right-0 z-10 w-2/3 bg-slate-200/[0.5] transition-all dark:bg-gray-700/[0.5] backdrop-blur-[50px] ${
           open ? "translate-x-0" : "translate-x-full"
-        }`}
+        } md:static md:transform-none md:dark:bg-transparent md:bg-transparent md:pl-3 md:backdrop-blur-none`}
       >
-        <ul>{listItems}</ul>
+        <ul className="pt-32 pl-10 flex flex-col gap-6 md:p-0 md:flex-row">{listItems}</ul>
       </nav>
     </header>
   );
