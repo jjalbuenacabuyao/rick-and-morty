69462a1a682b97d632d17ce46293eebdbbf76a5d
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-08 18:38:03 +0800

    adjust mt and gap in large devices

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -14,7 +14,7 @@ const Box = ({
   return (
     <>
       <Header />
-      <div className="flex flex-col gap-6 mx-36-140 mt-28 mb-8 lg:grid lg:grid-cols-[1fr_2fr]">
+      <div className="flex flex-col gap-6 mx-36-140 mt-28 mb-8 lg:grid lg:grid-cols-[1fr_2fr] lg:gap-10 lg:gap-x-5 lg:mt-36">
         <h1 className="text-center text-2xl font-bold text-sky-900 dark:text-sky-400 lg:col-span-2">
           {title}
         </h1>
