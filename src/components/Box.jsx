a8c04ce2a4bbe7c8f708d20d3f783310713d41da
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-08 18:26:03 +0800

    change layout in large screen devices

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -14,8 +14,8 @@ const Box = ({
   return (
     <>
       <Header />
-      <div className="flex flex-col gap-6 mx-36-140 mt-28 mb-8">
-        <h1 className="text-center text-2xl font-bold text-sky-900 dark:text-sky-400">
+      <div className="flex flex-col gap-6 mx-36-140 mt-28 mb-8 lg:grid lg:grid-cols-[1fr_2fr]">
+        <h1 className="text-center text-2xl font-bold text-sky-900 dark:text-sky-400 lg:col-span-2">
           {title}
         </h1>
         <Search setSearch={setSearch} setPage={setPage}/>
