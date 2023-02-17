164104cbae704cdf2398c38c08ffcc58b9ebf0e4
Joel Cabuyao <eustascaptainkid01@gmail.com>
2023-02-17 20:05:47 +0800

    basic boilerplate for episodes

--- src/containers/Episodes.jsx
+++ src/containers/Episodes.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, {useEffect, useState} from 'react'
+import { Header, Search } from '../components';
 
 const Episodes = () => {
+  let [page, setPage] = useState(1);
+  let [apiData, setApiData] = useState([]);
+  let [search, setSearch] = useState("");
+  let apiURL = `https://rickandmortyapi.com/api/episode/?name=${search}`;
+  const [pageInfo, setPageInfo] = useState([])
+
+  useEffect(() => {
+    (async function () {
+      let response = await fetch(apiURL).then((res) => res.json());
+      let { info, results } = response;
+      setApiData(results);
+      setPageInfo(info)
+    })();
+  }, [apiURL]);
+
+  let episodeCard = apiData.map((episode) => (
+    <div key={episode.id}>
+      <h2>{episode.name}</h2>
+      <p>{episode.episode}</p>
+      <p>{episode.air_date}</p>
+    </div>
+  ))
+
   return (
     <>
     <Header />
-    <div>Episodes</div>
+    <main className="flex flex-col gap-6 mx-36-140 mt-28 mb-12 lg:grid lg:grid-cols-[1fr_2fr] lg:gap-10 lg:gap-x-5 lg:mt-36 md:mb-16">
+      <h1 className="text-center text-2xl font-bold text-sky-900 dark:text-sky-400 lg:col-span-2 md:text-3xl lg:text-4xl">Episodes</h1>
+      <Search setSearch={search} setPage={setPage} />
+      {episodeCard}
+    </main>
     </>
     
   )
